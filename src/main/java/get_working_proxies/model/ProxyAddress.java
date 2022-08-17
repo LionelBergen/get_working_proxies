1aa3aba1afe628468128222b7880a9f4816a4e19
dustytrash <Lionelbergen@live.com>
2022-08-17 15:49:13 -0500

    added ProxyAddress as a model class

---
+++ src/main/java/get_working_proxies/model/ProxyAddress.java
@@ -0,0 +1,19 @@
+package get_working_proxies.model;
+
+public class ProxyAddress {
+	private String ipAddress;
+	private int port;
+	
+	public String getIpAddress() {
+		return ipAddress;
+	}
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+}
