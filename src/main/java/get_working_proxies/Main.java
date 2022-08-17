42aa40e24c3e679c4fb0fce90f28001235f0f93e
dustytrash <Lionelbergen@live.com>
2022-08-17 16:04:44 -0500

    Added spotless

--- src/main/java/get_working_proxies/Main.java
+++ src/main/java/get_working_proxies/Main.java
@@ -1,18 +1,17 @@
 package get_working_proxies;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Starting program");
-		
-		WebDriverManager.firefoxdriver().setup();
-		WebDriver driver = new FirefoxDriver();
-		driver.get("http://www.google.com");
-		
-		System.out.println("Finished program");
-	}
+  public static void main(String[] args) {
+    System.out.println("Starting program");
+
+    WebDriverManager.firefoxdriver().setup();
+    WebDriver driver = new FirefoxDriver();
+    driver.get("http://www.google.com");
+
+    System.out.println("Finished program");
+  }
 }
--- src/main/java/get_working_proxies/modal/ProxyAddress.java
+++ src/main/java/get_working_proxies/modal/ProxyAddress.java
@@ -1,19 +1,22 @@
 package get_working_proxies.modal;
 
 public class ProxyAddress {
-	private String ipAddress;
-	private int port;
-	
-	public String getIpAddress() {
-		return ipAddress;
-	}
-	public void setIpAddress(String ipAddress) {
-		this.ipAddress = ipAddress;
-	}
-	public int getPort() {
-		return port;
-	}
-	public void setPort(int port) {
-		this.port = port;
-	}
+  private String ipAddress;
+  private int port;
+
+  public String getIpAddress() {
+    return ipAddress;
+  }
+
+  public void setIpAddress(String ipAddress) {
+    this.ipAddress = ipAddress;
+  }
+
+  public int getPort() {
+    return port;
+  }
+
+  public void setPort(int port) {
+    this.port = port;
+  }
 }
